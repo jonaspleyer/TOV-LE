f17675a6382aaa8bef63fe865557298cf878bd9e
Jonas P <jonaspleyer@gmx.de>
2020-11-05 23:45:10 +0100

    Added Possibility to plot LE Results one by one (for up to 4 different exponents)

---
+++ Aspects/LE-SingleSolve.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Only use this to import the package DiffEqSolver 
+# from ../Solvers/ correctly
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from Solvers.Solver import DiffEqSolver
+import matplotlib.pyplot as plt
+
+class Plotter(DiffEqSolver):
+	def solveAndPlotResults(self, xi0, T0, dT0, xi_max, dxi, exponents, suppressWarning=False):
+		# Define linestyles for later plotting
+		linestyles = ['-', '--', '-.', ':']
+		
+		# Define arrays that store the information for different runs
+		results = [[]]*len(exponents)
+		succ = [[]]*len(exponents)
+		xi_end = [[]]*len(exponents)
+		
+		for i, exponent in enumerate(exponents):
+			results[i], succ[i], xi_end[i] = self.solveLE(xi0, T0, dT0, xi_max, dxi, exponent=exponent, suppressWarning=suppressWarning)
+			# Check if the solving was successful
+			if succ[i] == True:
+				# Plot Theta
+				plt.plot(results[i][:, 0], results[i][:, 1], label=r'$\theta(\xi)$ for $n=$' + str(exponent), linestyle=linestyles[i], c='black')
+			else:
+				print("Solving was not possible for $n=$"+str(exponent))
+		plt.legend()
+		plt.savefig('pictures/LE-SingleSolve.svg')
+		plt.show()
+
+# Define inital values
+xi0 = 0
+T0 = 1
+dT0 = 0
+xi_max = 1000
+dxi = 0.01
+
+# Create an instance of the Solver with polytropic EOS
+# Both of these values do not matter
+n = 2
+gamma = 1+1/n
+A = 1
+
+# Define a range of exponents to plot functions for (maximum 4)
+exponents = [1,2,3,4]
+
+# Initialise Solver
+Solver = Plotter(gamma, A)
+
+Solver.solveAndPlotResults(xi0, T0, dT0, xi_max, dxi, exponents, suppressWarning=False)
