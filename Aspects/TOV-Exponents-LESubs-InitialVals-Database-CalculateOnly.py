34e2cf41078e4e8142f2f53196abd6b4f2990895
Jonas P <jonaspleyer@gmx.de>
2020-12-02 17:36:43 +0100

    Huge Code cleanup and efficiency improvement. Limiting factor now seems to be database access. But scales linearly. So this is fine.

--- Aspects/TOV-Exponents-LESubs-InitialVals-Database-CalculateOnly.py
+++ Aspects/TOV-Exponents-LESubs-InitialVals-Database-CalculateOnly.py
@@ -15,9 +15,10 @@ import numpy as np
 import multiprocessing
 import ctypes
 import pymongo
+import signal
 
 class TOVLECalculator(DiffEqSolverLESubs):
-	def solveMultiprocExponents(self, n_0, n_max, n_step, A_initials, p0_initials, r0, u0, p0, R, rend, dr, suppressOutput=False, N_threads = 2, N_terms=1):
+	def solveMultiprocExponents(self, n_0, n_max, n_step, A_initials, p0_initials, r0, u0, p0, R, rend, dr, N_threads = 2, N_terms=1):
 		# Define linestyles for plotting later
 		linestyles = ['-', '--', '-.', ':']
 		
@@ -30,22 +31,22 @@ class TOVLECalculator(DiffEqSolverLESubs):
 		N_p0_initial = len(self.p0_initial_vals)
 		
 		# Define multiprocessing arrays that can be accessed even if processes run in parallel
-		self.r_maxes_all = multiprocessing.Array(ctypes.c_float, N_exponents*(N_terms+1)*N_A_initial*N_p0_initial)
-		self.r_Bool_all = multiprocessing.Array(ctypes.c_float, N_exponents*(N_terms+1)*N_A_initial*N_p0_initial)
+# 		self.r_maxes_all = multiprocessing.Array(ctypes.c_float, N_exponents*(N_terms+1)*N_A_initial*N_p0_initial)
+# 		self.r_Bool_all = multiprocessing.Array(ctypes.c_float, N_exponents*(N_terms+1)*N_A_initial*N_p0_initial)
 		self.exponents_all = multiprocessing.Array(ctypes.c_float, N_exponents*(N_terms+1)*N_A_initial*N_p0_initial)
-# 		self.A_initials_all = multiprocessing.Array(ctypes.c_float, N_exponents*(N_terms+1)*N_A_initial*N_p0_initial)
-# 		self.p0_initials_all = multiprocessing.Array(ctypes.c_float, N_exponents*(N_terms+1)*N_A_initial*N_p0_initial)
-		self.xi_maxes_all = multiprocessing.Array(ctypes.c_float, N_exponents*N_A_initial*N_p0_initial)
-		self.xi_Bool_all = multiprocessing.Array(ctypes.c_float, N_exponents*N_A_initial*N_p0_initial)
+		self.TOVCounter = multiprocessing.Array(ctypes.c_int, (N_terms+1)*N_A_initial*N_p0_initial)
+		self.LECounter = multiprocessing.Array(ctypes.c_int, N_A_initial*N_p0_initial)
+# 		self.xi_maxes_all = multiprocessing.Array(ctypes.c_float, N_exponents*N_A_initial*N_p0_initial)
+# 		self.xi_Bool_all = multiprocessing.Array(ctypes.c_float, N_exponents*N_A_initial*N_p0_initial)
 		
-		self.xi_maxes = multiprocessing.Array('f',[0 for i in range(0,N_exponents)])
-		self.xi_Bool = multiprocessing.Array('l',[0 for i in range(0,N_exponents)])
-		self.times = multiprocessing.Array('f',[0 for i in range(0,N_exponents)])
+		self.Interruptor = multiprocessing.Value(ctypes.c_int, 0)
+		self.Interruptor = 0
+		self.times = multiprocessing.Array(ctypes.c_float,N_exponents)
 		
 		self.counter = multiprocessing.Value(ctypes.c_int, 0)
 		self.update_counter = multiprocessing.Value(ctypes.c_int, 0)
 		# Initialise threads; Create list that contains all processes
-		processes = []
+		self.processes = []
 		
 		# Split process in several subthreads
 		# and distribute load equally over all threads
@@ -63,26 +64,28 @@ class TOVLECalculator(DiffEqSolverLESubs):
 			# Generate the process
 			x = multiprocessing.Process(target=self.solveForexponent_vals, args=(exponents, r0, u0, p0, R, rend, dr, N_terms, ))
 			# Store x in the processes array
-			processes.append(x)
+			self.processes.append(x)
 			# Actually start solving the equations in different processes
 			x.start()
 		
 		# Join the processes to have them end continue the method at the time when all processes have finished
-		for x in processes:
+		for x in self.processes:
 			x.join()
-		
-		print("\n")
-		
-		for i, val in enumerate(self.r_maxes_all):
-			if suppressOutput == False:
-				print("r_end=" + str(round(val,3)) + "   at n=" + str(round(self.exponents_all[i],3)) + "   with A=" + str(round(self.A_initials_all[i],3)) + "   with p0=" + str(round(self.p0_initials_all[i],3)))
 		
 		# Stop the timer
 		end = time.time()
 		
 		# Print Duration
-		print("===== Finished Process =====")
-		print("Duration was " + str(round(end-start,3)) + " Seconds for " + str(N_exponents*(N_terms+2)*N_A_initial*N_p0_initial) + " Test Samples with " + str(N_threads) + " threads")
+		print("\n===== Finished Process =====")
+		print("Duration was " + str(round(end-start,3)) + " Seconds for " + str(self.counter.value*(N_terms+2)) + " Test Samples with " + str(N_threads) + " threads")
+		
+	def terminateAll(self):
+		self.Interruptor = 1
+		print("\n============================")
+		print("======= Terminating! =======")
+		print("============================")
+		for p in self.processes:
+			p.terminate()
 		
 	def solveForexponent_vals(self, exponents_index, r0, u0, p0, R, rend, dr, N_terms):
 		# Create a client to talk to mongodb
@@ -115,45 +118,51 @@ class TOVLECalculator(DiffEqSolverLESubs):
 					for j in range(0,N_terms+1):
 						# Check if in this particular calculation previously an integration was running over the limit of r_end
 						# If this is the case we no longer need to solve since we know that the zero value will only increase.
-						if self.checkCalculateResult(exponents_collection, Solver.exponent_vals[i], j, A_init, p_init, 'TOV', rend)==True:
+						if self.checkCalculateResult(exponents_collection, Solver.exponent_vals[i], j, A_init, p_init, 'TOV', rend, dr)==True:
 							# Checks if the solving routine before encountered any results that could not be solved.
-							if i>=2 and 2 == sum([0==self.r_Bool_all[j*N_exponents*N_A_initial*N_p0_initial + k*N_exponents*N_p0_initial + l*N_exponents + h] for h in range(0,i-1)]) and 3 == sum([0==int(self.r_maxes_all[j*N_exponents*N_A_initial*N_p0_initial + k*N_exponents*N_p0_initial + l*N_exponents + h]>0) for h in range(0,i-1)]):
-								self.r_maxes_all[j*N_exponents*N_A_initial*N_p0_initial + k*N_exponents*N_p0_initial + l*N_exponents + i] = rend
-								self.r_Bool_all[j*N_exponents*N_A_initial*N_p0_initial + k*N_exponents*N_p0_initial + l*N_exponents + i] = 0
-								# Write here that solving was not successfull with the current parameters
-								self.updateDBResults(exponents_collection, Solver.exponent_vals[i], j, A_init, p_init, rend, rend, dr, equ_type='TOV', succ=False)
+							if self.TOVCounter[j*N_A_initial*N_p0_initial + k*N_p0_initial + l] >= 2:
+								self.TOVCounter[j*N_A_initial*N_p0_initial + k*N_p0_initial + l] += 1
+								# Just set r_max[j] to rend. This will yield the correct solving result
+								r_max[j] = rend
 							# If the previous case was not fulfilled we need to calculate the result.
 							else:
 								results_TOV[j], results_TOV_small[j], succ_TOV[j], r_max[j] = Solver.solveTOV(r0, u0, p_init, R, rend, dr, terms=j, exponent=Solver.exponent_vals[i], suppressWarning=True)
-								self.r_maxes_all[j*N_exponents*N_A_initial*N_p0_initial + k*N_exponents*N_p0_initial + l*N_exponents + i] = r_max[j]
-								self.r_Bool_all[j*N_exponents*N_A_initial*N_p0_initial + k*N_exponents*N_p0_initial + l*N_exponents + i] = 1 if r_max[j] < rend else 0
+								self.TOVCounter[j*N_A_initial*N_p0_initial + k*N_p0_initial + l] += int(r_max[j]>=rend)
 							
 								# Update the results already present
-								self.updateDBResults(exponents_collection, Solver.exponent_vals[i], j, A_init, p_init, r_max[j], rend, dr, equ_type='TOV', succ=bool(r_max[j]<rend))
+							self.updateDBResults(exponents_collection, Solver.exponent_vals[i], j, A_init, p_init, r_max[j], rend, dr, equ_type='TOV', succ=bool(r_max[j]<rend))
 								
-					if self.checkCalculateResult(exponents_collection, Solver.exponent_vals[i], 0, A_init, p_init, 'LE', rend) == True:
-						# Get the LE results. We do not need to iterate over different terms.
-						results_LE, succ_LE, xi_max = Solver.convertSolveLE(r0, u0, p_init, R, rend, dr, exponent=Solver.exponent_vals[i], suppressWarning=True, suppressOutput=True, nointerpolate=True)
-						self.xi_maxes_all[k*N_exponents*N_p0_initial + l*N_exponents + i] = xi_max
-						self.xi_Bool_all[k*N_exponents*N_p0_initial + l*N_exponents + i] = 1 if xi_max < rend else 0
+					if self.checkCalculateResult(exponents_collection, Solver.exponent_vals[i], 0, A_init, p_init, 'LE', rend, dr) == True:
+						# Check if we need to solve the LE equation or if previous attempts failed
+						if self.LECounter[k*N_p0_initial+ l] >= 2:
+							self.LECounter[k*N_p0_initial+ l] += 1
+							xi_max = rend
+						else:
+							# Get the LE results. We do not need to iterate over different terms.
+							results_LE, succ_LE, xi_max = Solver.convertSolveLE(r0, u0, p_init, R, rend, dr, exponent=Solver.exponent_vals[i], suppressWarning=True, suppressOutput=True, nointerpolate=True)
+							self.LECounter[k*N_p0_initial+ l] += int(xi_max>=rend)
 						self.updateDBResults(exponents_collection, Solver.exponent_vals[i], 0, A_init, p_init, xi_max, rend, dr, equ_type='LE', succ=xi_max<rend)
 						
 					# Set the counter value for display plus one and append the time it took to calculate.
 					self.counter.value = self.counter.value + 1
 					self.times[i] = time.time()-A
-					print("\rFinished " + str(self.counter.value) + "/" + str(N_exponents*N_A_initial*N_p0_initial) + " and updated " + str(round(self.update_counter.value/(N_exponents*(N_terms+2)*N_A_initial*N_p0_initial)*100,1)) + "% exponents. Last solved exponent was n=" + str(round(self.exponent_vals[i],3)) + " and time t=" + str(round(self.times[i],3)), end='')
+					print("\rFinished " + str(self.counter.value) + "/" + str(N_exponents*N_A_initial*N_p0_initial) 
+		   + " and updated " + str(round(self.update_counter.value/(N_exponents*(N_terms+2)*N_A_initial*N_p0_initial)*100,1)) 
+		   + "% exponents and cut " + str(len([c for c in self.TOVCounter if c>0])+len([c for c in self.LECounter if c>0])) + "/" + str(len(self.TOVCounter)+len(self.LECounter))
+		   + " curves. Last solved exponent was n=" + str(round(self.exponent_vals[i],3)) + " and time t=" 
+		   + str(round(self.times[i],3)), end='')
 			 
 	def updateDBResults(self, collection, n, term, A_init, p_init, r_max, r_end, dr, equ_type, succ):
 		# Create the details dict with additional information about the solving process
 		details_dict = {'r_end':float(r_end), 'dr':float(dr)}
-		# Create the succ variable that defines if the solving was successfull in determining the zero value of the function
-		succ = bool(r_max<r_end)
+		# Solving was succesfull if the r_max value is between dr and r_end
+		succ = bool(r_max<r_end and r_max>dr)
 		# Create the save dict. The structure with all necessary information that will be saved
 		save_dict = {"n":float(n),"terms":int(term),"A_init":float(A_init),'p_init':float(p_init),'r_max':float(r_max), 'equ_type':equ_type, 'succ':succ,'details':details_dict}
 		# Search the database for already existing entries
-		presents = list(collection.find({"n":float(n),"terms":int(term),"A_init":float(A_init),'p_init':float(p_init),'r_max':float(r_max), 'equ_type':equ_type}))
+		presents = list(collection.find({"n":float(n),"terms":int(term),"A_init":float(A_init),'p_init':float(p_init), 'equ_type':equ_type}))
 		# If there are no entries, just save the newly calculated result
-		if len(presents)<1:
+		if len(presents)<1 and self.Interruptor == 0:
 			collection.insert_one(save_dict)
 			self.update_counter.value = self.update_counter.value + 1
 		# If there is one or more entries present with the same saving values, we will replace them. 
@@ -164,7 +173,7 @@ class TOVLECalculator(DiffEqSolverLESubs):
 			collection.insert_one(save_dict)
 			self.update_counter.value = self.update_counter.value + 1
 	
-	def checkCalculateResult(self, collection, n, term, A_init, p_init, equ_type, rend):
+	def checkCalculateResult(self, collection, n, term, A_init, p_init, equ_type, rend, dr):
 		# Check if a result should be newly calculated
 		# Returns TRUE if result should be calculated. Return False otherwise
 		results = list(collection.find({"n":float(n),"terms":int(term),"A_init":float(A_init),'p_init':float(p_init), 'equ_type':equ_type}))
@@ -204,9 +213,9 @@ Solver = TOVLECalculator(gamma, A)
 r0 = 0
 u0 = 0.0
 p0 = 1
-R  = 100
+R  = 10
 rend = R
-dr = 0.001
+dr = 0.01
 
 # Define the range of exponents to solve for
 n_0 = 0.01
@@ -214,10 +223,15 @@ n_max = 5.00
 n_step = 0.01
 
 # Define the range for initial values
-A_initials = [0.2,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2]
+A_initials = [0.025,0.05,0.1,0.2,0.4,0.8,1,2,4,8]
 
-p0_initials = [0.05,1.5,500]
+p0_initials = [0.05,0.1,0.2,0.4,0.8,1,2,4,8,16,32,64,128,256,512]
 
 print(time.strftime('%X %x %Z'))
 print("===== Starting Process =====")
-Solver.solveMultiprocExponents(n_0, n_max, n_step, A_initials, p0_initials, r0, u0, p0, R, rend, dr, suppressOutput=True, N_threads=6, N_terms=0)
+
+def signal_handler(sig, frame):
+	Solver.terminateAll()
+
+signal.signal(signal.SIGINT, signal_handler)
+Solver.solveMultiprocExponents(n_0, n_max, n_step, A_initials, p0_initials, r0, u0, p0, R, rend, dr, N_threads=15, N_terms=0)
