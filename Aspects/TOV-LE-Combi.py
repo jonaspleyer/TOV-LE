7396d73c223d9119ce12fa8174b2650a94696925
Jonas P <jonaspleyer@gmx.de>
2020-12-18 14:58:42 +0100

    Updated plots with m/r and average density m/r^3

--- Aspects/TOV-LE-Combi.py
+++ Aspects/TOV-LE-Combi.py
@@ -15,7 +15,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
 from Solvers.Solver import DiffEqSolver
-
+import Standards.PlottingStandards as Standards
 
 class Plotter(DiffEqSolver):
 	def solveAndPlotResults(self, r0, u0, p0, R, rend, dr):
@@ -31,30 +31,40 @@ class Plotter(DiffEqSolver):
 
 		# Plot the results
 		# Create a new figure with the right dimesions
-		plt.figure(figsize=[6.4,8])
+		cm = 1/2.54
+		plt.figure(figsize=[16*cm,12*cm])
 		
 		# Create a subplot for the different pressures and for the mass and density
-		plt.subplot(2,1,1)
-		
-		# Plot pressure TOV and LE
+		# Subplot for pressure
+		plt.subplot(2,2,1)
 		plt.plot(results_TOV[:, 0], results_TOV[:, 2], label=r'Pressure $p_{TOV}$', linestyle='-', c='black')
 		plt.plot(results_LE[:, 0], results_LE[:, 2], label=r'Pressure $p_{LE}$', linestyle=':', c='black')
-		
-		# Create a legend
+		# Create new y-ticks
 		plt.legend()
 		
-		# Create the next subplot
-		plt.subplot(2,1,2)
+		# Next subplot for Density
+		plt.subplot(2,2,2)
+		y_vals_TOV = [results_TOV[:,1][i]/results_TOV[:,0][i]**3/4/np.pi*3 if i >= 1 else np.nan for i in range(0,len(results_TOV[:,0]))]
+		y_vals_LE  = [results_LE[:,1][i]/results_LE[:,0][i]**3/4/np.pi*3 if i >= 1 else np.nan  for i in range(0,len(results_LE[:,0]))]
+		plt.plot(results_TOV[:,0], results_TOV[:,3], label=r'$\rho_{TOV}$', linestyle='-', c='black')
+		plt.plot(results_TOV[:,0], y_vals_TOV, label=r'$m_{TOV}/r^3$', linestyle='--', c='black')
+		plt.plot(results_LE[:,0], results_LE[:,3], label=r'$\rho_{LE}$', linestyle=':', c='black')
+		plt.plot(results_LE[:,0], y_vals_LE, label=r'$m_{LE}/r^3$', linestyle=(0,(1,5)), c='black')
+		plt.legend()
 		
-		# Plot the density for the LE and TOV solution
-		plt.plot(results_TOV[:,0], results_TOV[:,3], label=r'$\rho_{TOV}$', linestyle='-.', c='black')
-		plt.plot(results_LE[:,0], results_LE[:,3], label=r'$\rho_{LE}$', linestyle='--', c='black')
-		
-		# Plot m(r) TOV and LE
+		# Next subplot for Mass
+		plt.subplot(2,2,3)
 		plt.plot(results_TOV[:, 0], results_TOV[:, 1], label=r'$m_{TOV}$', linestyle='-', c='black')
 		plt.plot(results_LE[:,0],results_LE[:,1], label='$m_{LE}$', linestyle=':', c='black')
-
-		# Plot the legend
+		plt.legend()
+		
+		# Next subplot for m(r)/r**3
+		plt.subplot(2,2,4)
+		# Calculate new vals and normalise with self.eos(p0,0)
+		y_vals_TOV = [results_TOV[:,1][i]/results_TOV[:,0][i] if i >= 1 else np.nan for i in range(0,len(results_TOV[:,0]))]
+		y_vals_LE  = [results_LE[:,1][i]/results_LE[:,0][i] if i >= 1 else np.nan  for i in range(0,len(results_LE[:,0]))]
+		plt.plot(results_TOV[:,0], y_vals_TOV, label=r'$m_{TOV}/r$', linestyle='-', c='black')
+		plt.plot(results_LE[:,0], y_vals_LE, label=r'$m_{LE}/r$', linestyle=':', c='black')
 		plt.legend()
 
 		# Save the total picture
@@ -74,7 +84,7 @@ Solver = Plotter(gamma,A)
 r0 = 0
 u0 = 0
 p0 = 1
-R = 1.2
+R = 2.2
 rend = R
 dr = 0.01
 
