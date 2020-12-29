8a604e4cd4a738a404c2bbd56362e050ba610413
Jonas P <jonaspleyer@gmx.de>
2020-12-29 16:31:20 +0100

    Reimplemented standards.

---
+++ Standards/PlottingStandards.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 29 15:04:22 2020
+
+@author: jonas
+"""
+import matplotlib.pyplot as plt
+
+linestyles = [
+	'solid',
+	'dashed',
+	'dotted',
+	'dashdot',
+	(0, (5, 10)),
+	(0, (1, 10)),
+	(0, (3, 10, 1, 10)),
+	(0, (5, 1)),
+	(0, (1, 1)),
+	(0, (3, 1, 1, 1)),
+	(0, (3, 5, 1, 5)),
+	(0, (3, 5, 1, 5, 1, 5)),
+	(0, (3, 10, 1, 10, 1, 10)),
+	(0, (3, 1, 1, 1, 1, 1))]
+
+linecolours = [
+	'k',
+	'r',
+	'g',
+	'b'
+	] 
