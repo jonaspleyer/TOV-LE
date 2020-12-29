128a9a70b30dbb801104ef2b66bed600bcc9f941
Jonas P <jonaspleyer@gmx.de>
2020-12-29 16:30:59 +0100

    Added option to validate that solved equations are the same as exact results.

---
+++ Aspects/LE-ValidateSols.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Only use this to import the package DiffEqSolver 
+# from ../Solvers/ correctly
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from Solvers.Solver import DiffEqSolver
+from Standards import PlottingStandards as standards
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+class Plotter(DiffEqSolver):
+	def solveAndPlotResults(self, xi0, T0, dT0, xi_max, dxi, suppressWarning=False):
+		# Known exact solutions are for n=0,1,5
+		exponents = [0,1,5]
+		solutions = [lambda x:1-x**2/6, lambda x:np.sin(x)/x if x>0 else 1, lambda x: 1/(1+1/3*x**2)**(1/2)]
+		zerovals  = [np.sqrt(6), np.pi, xi_max]
+		# Define linestyles for later plotting
+		linestyles = standards.linestyles
+		
+		# Define arrays that store the information for different runs
+		results = [[]]*len(exponents)
+		succ = [[]]*len(exponents)
+		xi_end = [[]]*len(exponents)
+		
+		# Initialise plot with right size
+		cm = 1/2.54
+		plt.figure(figsize=[16*cm,12*cm])
+		
+		for i, exponent in enumerate(exponents):
+			results[i], succ[i], xi_end[i] = self.solveLE(xi0, T0, dT0, xi_max, dxi, exponent=exponent, suppressWarning=suppressWarning)
+			# Check if the solving was successful
+			if succ[i] == True:
+				plt.subplot(2, 2, 1)
+				plt.plot(results[i][:, 0], results[i][:, 1], label=r'$\theta_{calc}$ for $n=$' + str(exponent), linestyle=linestyles[i+1], c='black')
+				x_vals = np.linspace(xi0,zerovals[i])
+# 				plt.plot(x_vals, [solutions[i](x_val) for x_val in x_vals], label=r'$\theta_{exct}$ for $n=$' + str(exponent), linestyle=linestyles[i+4], c='k')
+				plt.legend()
+				plt.title(r'Exact Solutions for $n=0,1,5$')
+				
+				plt.subplot(2, 2, i+2)
+				diff = [results[i][:,1][j]-solutions[i](results[i][:,0][j]) for j in range(len(results[i][:,0]))]
+				plt.plot(results[i][:,0], diff, label=r'$\theta_{calc}-\theta_{exct}$', linestyle=standards.linestyles[1], c='k')
+				plt.legend()
+				plt.title(r'Difference for $n=$' + str(exponent))
+			else:
+				print("Solving was not possible for $n=$"+str(exponent))
+		plt.tight_layout()
+		plt.subplots_adjust(hspace=0.3)
+		plt.savefig('pictures/LE-ValidateSols.svg')
+		plt.show()
+		matplotlib.use("pgf")
+		matplotlib.rcParams.update({
+# 		    "pgf.texsystem": "pdflatex",
+		    'font.family': 'serif',
+		    'text.usetex': True,
+		    'pgf.rcfonts': False,
+		})
+		plt.savefig("pictures/LE-ValidateSols.pgf", dpi=1000, bbox_inches='tight')
+
+
+# Define inital values
+xi0 = 0
+T0 = 1
+dT0 = 0
+xi_max = 10
+dxi = 0.01
+
+# Create an instance of the Solver with polytropic EOS
+# Both of these values do not matter
+n = 2
+gamma = 1+1/n
+A = 1
+
+# Initialise Solver
+Solver = Plotter(gamma, A)
+
+Solver.solveAndPlotResults(xi0, T0, dT0, xi_max, dxi, suppressWarning=False)
