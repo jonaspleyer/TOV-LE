2415431d237059fc449634842b2feba05fc09b66
Jonas P <jonaspleyer@gmx.de>
2021-04-09 14:01:07 +0200

    changed styles for plotting to have it black and white.

--- Aspects/TOV-Exponents-LESubs-InitialVals-Database-PlotResults.py
+++ Aspects/TOV-Exponents-LESubs-InitialVals-Database-PlotResults.py
@@ -20,7 +20,15 @@ class Plotter():
 		self.coll = self.db["Exponents-polytropic-EOS"]
 		
 		# Create plotstyles
-		self.curve_styles = ['-', '--', '-.', ':']
+# 		self.curve_styles = ['-', '--', '-.', ':',(0, (1, 10)), (0, (1, 1))]
+		self.curve_styles = [
+			(0, (1, 2)),
+			(0, (7, 1, 4, 1)),
+			(0, (1, 4)),
+			(0, (5, 2, 1, 2)),
+			(0, (1, 6)),
+			(0, (3, 2, 1, 2, 1, 2))
+			]
 		self.curve_colours = ['k','r', 'g', 'b', 'orangered', 'lime','cornflowerblue']
 	
 	def plotInitialVals(self, var1_name, var1_vals, var2_name, var2_fixed_val, terms, ylim_low=None, ylim_up=None, xlim_low=None, xlim_up=None, noExport=True):
@@ -63,12 +71,20 @@ class Plotter():
 		else:
 			var1_name=r'A'
 		
+		# Initialise plot with right size
+		cm = 1/2.54
+		plt.figure(figsize=[16*cm,9*cm])
+		
 		# Create the plot axis
 		fig, axs = plt.subplots()
 		for i, var1_val in enumerate(var1_vals):
-			axs.plot(plot_results_LE[var1_val][:,0], plot_results_LE[var1_val][:,1], linestyle=self.curve_styles[0], c=self.curve_colours[i],  label="LE for " + var1_name + "=" + str(var1_val))
+			axs.plot(plot_results_LE[var1_val][:,0], plot_results_LE[var1_val][:,1], linestyle=self.curve_styles[2*i], c=self.curve_colours[0],  label="LE for " + var1_name + "=" + str(var1_val))
 			for term in terms:
-				axs.plot(plot_results_TOV[term][var1_val][:,0], plot_results_TOV[term][var1_val][:,1], linestyle=self.curve_styles[1+term], c=self.curve_colours[i], label="TOV with term=" + str(term) + " for " + var1_name + "=" + str(var1_val))
+				if len(terms)==1:
+					label = "TOV for " + var1_name + "=" + str(var1_val)
+				else:
+					label = "TOV with term=" + str(term) + " for " + var1_name + "=" + str(var1_val)
+				axs.plot(plot_results_TOV[term][var1_val][:,0], plot_results_TOV[term][var1_val][:,1], linestyle=self.curve_styles[2*i+1], c=self.curve_colours[0], label=label)
 		axs.set_yscale('log')
 		
 		axs.set_ylabel(r'$r_ 0$')
@@ -77,10 +93,11 @@ class Plotter():
 		axs.set_xlabel(r'$n=\frac{1}{\gamma-1}$')
 		
 		# Resize the whole plot to fit the legend next to it.
-		box = axs.get_position()
-		axs.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+# 		box = axs.get_position()
+# 		axs.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 		# Create the legend right of plot
-		axs.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# 		axs.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+		axs.legend()
 		plt.tight_layout()
 		
 		if var2_name=="p_init":
@@ -107,4 +124,4 @@ class Plotter():
 
 Plodda = Plotter()
 
-Plodda.plotInitialVals(var1_name='p_init', var1_vals=[0.1,1,8], var2_name='A_init', var2_fixed_val=1, terms=[0], xlim_low=0, ylim_up=None, noExport=False)
+Plodda.plotInitialVals(var1_name='p_init', var1_vals=[0.1,1], var2_name='A_init', var2_fixed_val=1, terms=[0], xlim_low=0, ylim_up=None, noExport=False)
