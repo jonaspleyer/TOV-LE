67a5922537287a63306fee39128b675d90855f43
Jonas P <jonaspleyer@gmx.de>
2021-06-15 16:46:20 +0200

    Updated makefile to have nicer structure.

--- CPP-Code/Solvers/make.sh
+++ CPP-Code/Solvers/make.sh
@@ -1,1 +1,11 @@
-gcc Solver.cpp -o SolverBIN -lstdc++ -lm -ldl
+OUTPUT_FILE=SolverBIN
+INPUT_FILE=Solver.cpp
+
+OPTIONS=(-Wall -lstdc++ -lm -ldl)
+
+INCLUDED_LIBRARIES=(libmongocxx)
+
+echo "Compiling $INPUT_FILE"
+echo "Include flags $(pkg-config --cflags --libs ${INCLUDED_LIBRARIES[@]})"
+time gcc $INPUT_FILE -o $OUTPUT_FILE ${OPTIONS[@]} $(pkg-config --cflags --libs ${INCLUDED_LIBRARIES[@]})
+echo "Written to $OUTPUT_FILE"
